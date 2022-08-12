36b7e827a5e19a208fe38f8a767fd0bee207c3ca
Sreejith S <sreejithsev@gmail.com>
2022-08-12 11:38:18 +0530

    Add actuator

---
+++ src/main/java/com/dev/simpleapi/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.dev.simpleapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/api")
+    public String getGreeting(){
+        return "Hello World!";
+    }
+}
